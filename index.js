e1ba7d9635e71e6f1026622d1918e6bb00dbdb9e
Sathvik Balaji <sathvik@Sathviks-MacBook-Pro.local>
2017-08-05 10:02:59 -0700

    added pictures and formatting for website.

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
+
+//routing
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/resources/html/homepage.html')
+})
+app.get('/soccer', (req, res) => {
+  res.sendFile(__dirname + '/resources/html/soccerpage.html')
+})
+app.get('/cello', (req, res) => {
+  res.sendFile(__dirname + '/resources/html/cello.html')
+})
+app.get('/summer/2017', (req, res) => {
+  res.sendFile(__dirname + '/resources/html/summer2017.html')
+})
+app.get('/header', (req, res) => {
+  res.sendFile(__dirname + '/resources/html/header.html')
+})
+
+//scripts/pictures/stylesheets
+app.get('/bootstrap/css', (req, res) => {
+  res.sendFile(__dirname + '/resources/css/bootstrap.min.css')
+})
+app.get('/bootstrap/js', (req, res) => {
+  res.sendFile(__dirname + '/resources/js/bootstrap.min.js')
+})
+app.get('/jquery', (req, res) => {
+  res.sendFile(__dirname + '/resources/js/jquery-3.2.1.min.js')
+})
+app.get('/galileo', (req, res) => {
+  res.sendFile(__dirname + '/resources/etc/galileo.jpg')
+})
+app.get('/beach', (req, res) => {
+  res.sendFile(__dirname + '/resources/etc/sathvik_beach.jpg')
+})
+app.get('/soccer_background', (req, res) => {
+  res.sendFile(__dirname + '/resources/etc/soccer.jpg')
+})
+app.get('/cello_background', (req, res) => {
+  res.sendFile(__dirname + '/resources/etc/cello.jpg')
+})
+app.get('/camp_background', (req, res) => {
+  res.sendFile(__dirname + '/resources/etc/camp.jpg')
+})
+app.get('/css/header', (req, res) => {
+  res.sendFile(__dirname + '/resources/css/header.css')
+})
